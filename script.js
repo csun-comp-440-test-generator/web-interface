c5f5fcfc71222c0caa6c81c04b0bf0eaf00ad65a
Plixelated <76276169+Plixelated@users.noreply.github.com>
2025-04-08 11:23:13 -0700

    Added get request

--- script.js
+++ script.js
@@ -20,27 +20,54 @@ function login() {
   }
 }
 
+//INSTRUCTOR.HTML
 function getCourses(userId) {
+  //Replace with FETCH Reqeust
   return JSON.parse(localStorage.getItem("courses_" + userId) || "[]");
 }
 
 function saveCourses(userId, courses) {
+  //Replace wtih POST request
   localStorage.setItem("courses_" + userId, JSON.stringify(courses));
 }
 
-function addCourse(name) {
-  let userId = localStorage.getItem("userId");
-  let courses = getCourses(userId);
-  if (!courses.includes(name)) {
-    courses.push(name);
-    saveCourses(userId, courses);
+//COURSE.HTML
+async function addCourse(sectionID, courseID, teacherID, assistantID) {
+  const body = {
+    section_id:sectionID,
+    course_id:courseID,
+    teacher_id:teacherID,
+  };
+
+  if (assistantID !== null){
+    body.assistant_id = assistantID;
+  }
+
+  const url = 'http://127.0.0.1:8000/course/get_courses'
+  try{
+    const res = await fetch(url,{
+      method:'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({body})
+    })
+    if (!res.ok){
+      throw new Error(`Response status: ${res.status}`);
+    }
+    const json=await res.json();
+    return json
+  } catch(error){
+    console.log(error.message);
   }
 }
-
+//STUDENT.HTML
 function getEnrolledCourses(id) {
+  //replace wtih FETCH request
   return JSON.parse(localStorage.getItem("enrolled_" + id) || "[]");
 }
-
+//ENROLL.HTML
+//Replace with POST
 function enrollStudent(id, course) {
   let list = getEnrolledCourses(id);
   if (!list.includes(course)) {
@@ -48,18 +75,40 @@ function enrollStudent(id, course) {
     localStorage.setItem("enrolled_" + id, JSON.stringify(list));
   }
 }
-
+//INSTRUCTOR.HTML
+//STUDENT.HTML
 function getExams(userId) {
+  //Replace with FETCH request
   return JSON.parse(localStorage.getItem("exams_" + userId) || "[]");
 }
 
 function saveExams(userId, exams) {
+  //Replace with POST request
   localStorage.setItem("exams_" + userId, JSON.stringify(exams));
 }
-
+//EXAM.HTML
 function addExam(title, questions, course, timeLimit) {
   let userId = localStorage.getItem("userId");
   let exams = getExams(userId);
   exams.push({ title, questions, course, timeLimit });
   saveExams(userId, exams);
+}
+
+async function getClasses(){
+  const url = 'http://127.0.0.1:8000/course/get_courses'
+  try{
+    const res = await fetch(url,{
+      method:'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    })
+    if (!res.ok){
+      throw new Error(`Response status: ${res.status}`);
+    }
+    const json=await res.json();
+    return json
+  } catch(error){
+    console.log(error.message);
+  }
 }
